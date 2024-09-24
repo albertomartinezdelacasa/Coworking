9201f56957d9c7f3cb886909a521671449999dc0
Mauro Oliver <olivermauro@outlook.com>
2024-09-24 21:02:11 +0200

    run dev y run initdb funcionando

--- server/src/db/getPool.js
+++ server/src/db/getPool.js
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import mysql from 'mysql2/promise';
 
 const { MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB } = process.env;
@@ -7,17 +9,27 @@ let pool;
 const getPool = async () => {
     try {
         if (!pool) {
-            pool = mysql.createConnection({
+            // Creamos un pool temporal.
+            pool = mysql.createPool({
+                host: MYSQL_HOST,
+                user: MYSQL_USER,
+                password: MYSQL_PASS,
+                timezone: 'Z',
+            });
+
+            // Con la conexión anterior creamos la base de datos si no existe.
+            await pool.query(`CREATE DATABASE IF NOT EXISTS ${MYSQL_DB}`);
+
+            // Creamos el pool definitivo.
+            pool = mysql.createPool({
                 host: MYSQL_HOST,
                 user: MYSQL_USER,
                 password: MYSQL_PASS,
                 database: MYSQL_DB,
                 timezone: 'Z',
             });
-
-            await pool.query(`CREATE DATABASE IF NOT EXISTS ${MYSQL_DB}`);
-            await pool.query(`USE ${MYSQL_DB}`);
         }
+
         return await pool;
     } catch (err) {
         console.error(err);
