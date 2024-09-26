9c844d6342e03121662f5cf1639966f7c5a5bbff
Alberto Martinez <albertomgraupera@gmail.com>
2024-09-26 10:37:27 +0200

    userscontrollers y sendemailUtil

--- server/src/controllers/users/index.js
+++ server/src/controllers/users/index.js
@@ -1,4 +1,10 @@
 // Aqui se importan los controladores de la carpeta users
 import activateUserController from './activateUserController.js';
+import newUserController from './newUserController.js';
+import loginUserController from './loginUserController.js';
+import getProfileUserController from './getProfileUserController.js';
 // Aqui se exportan los controladores
 export default activateUserController;
+export default newUserController;
+export default loginUserController;
+export default getProfileUserController;
