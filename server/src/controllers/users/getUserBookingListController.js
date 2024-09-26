2acd85a0af86f989571e573a76467fad1ab4ac85
JoshbaSov <josebasaldana@gmail.com>
2024-09-26 12:17:20 +0200

    lista de reservas del usuario (getUserBookingList) añadido

---
+++ server/src/controllers/users/getUserBookingListController.js
@@ -0,0 +1,53 @@
+// Importamos la función que retorna una conexión con la base de datos.
+import getPool from '../../db/getPool.js';
+
+// Función controladora que retorna el listado de reservas.
+const listUserBookingsController = async (req, res, next) => {
+    try {
+        // Obtenemos los datos del usuario necesarios.
+        let searchingUser = req.user.id;
+
+        // Obtenemos una conexión con la base de datos.
+        const pool = await getPool();
+
+        // Obtenemos el listado de reservas.
+        const [bookings] = await pool.query(
+            `
+            SELECT  
+                b.id,
+                b.idUser,
+                b.idOffice,
+                b.checkIn,
+                b.checkOut,
+                b.guests,
+                b.status,
+                b.createdAt,
+                u.username AS searchingUser,
+
+            FROM bookings b
+            INNER JOIN users u ON u.id = b.idUser
+            INNER JOIN offices o ON o.id = b.idOffice
+            WHERE u.username LIKE ?
+            GROUP BY b.id 
+            `,
+            /*
+                INNER JOIN officeEquipments oe ON oe.id = o.idOffice
+                    INNER JOIN equipments e ON e.id = oe.idEquipment
+                INNER JOIN officePhotos op ON op.id = o.idOffice
+                */
+            [searchingUser],
+        );
+
+        // Enviamos una respuesta al cliente.
+        res.send({
+            status: 'ok',
+            data: {
+                bookings,
+            },
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+export default listUserBookingsController;
