c0e05efaa8b374c415417f8f8848d597953cc762
Claudio Anton <clau.anton98@gmail.com>
2024-10-22 19:20:34 +0200

    CSS Crear Oficina

--- client/src/pages/AddOfficeAdminPage.jsx
+++ client/src/pages/AddOfficeAdminPage.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../contexts/AuthContext';
-import toast from 'react-hot-toast';
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthContext";
+import toast from "react-hot-toast";
 
 // Obtención de la URL de la API desde las variables de entorno
 const { VITE_API_URL } = import.meta.env;
@@ -11,17 +11,18 @@ const AddOfficeAdminPage = () => {
   const navigate = useNavigate();
 
   // Estados para los campos del formulario
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [address, setAddress] = useState('');
-  const [workspace, setWorkspace] = useState('');
-  const [capacity, setCapacity] = useState('');
-  const [price, setPrice] = useState('');
-  const [opening, setOpening] = useState('');
-  const [closing, setClosing] = useState('');
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [address, setAddress] = useState("");
+  const [workspace, setWorkspace] = useState("");
+  const [capacity, setCapacity] = useState("");
+  const [price, setPrice] = useState("");
+  const [opening, setOpening] = useState("");
+  const [closing, setClosing] = useState("");
   const [photos, setPhotos] = useState([]);
   const [equipments, setEquipments] = useState([]); // Estado para los equipamientos
   const [selectedEquipments, setSelectedEquipments] = useState([]); // Equipamientos seleccionados
+  const [photoPreview, setPhotoPreview] = useState([]);
 
   // Obtener equipamientos al montar el componente
   useEffect(() => {
@@ -34,11 +35,11 @@ const AddOfficeAdminPage = () => {
         });
 
         if (!res.ok) {
-          throw new Error('Error al obtener los equipamientos'); // Manejar error si no es 2xx
+          throw new Error("Error al obtener los equipamientos"); // Manejar error si no es 2xx
         }
 
         const body = await res.json();
-        if (body.status === 'ok') {
+        if (body.status === "ok") {
           setEquipments(body.data.equipments[0]); // Asegúrate de que esta es la estructura correcta
         } else {
           toast.error(body.message);
@@ -61,17 +62,17 @@ const AddOfficeAdminPage = () => {
 
       // Creación del objeto FormData para enviar los datos
       const formData = new FormData();
-      formData.append('name', name);
-      formData.append('description', description);
-      formData.append('address', address);
-      formData.append('workspace', workspace);
-      formData.append('capacity', capacity);
-      formData.append('price', price);
-      formData.append('opening', formattedOpening);
-      formData.append('closing', formattedClosing);
+      formData.append("name", name);
+      formData.append("description", description);
+      formData.append("address", address);
+      formData.append("workspace", workspace);
+      formData.append("capacity", capacity);
+      formData.append("price", price);
+      formData.append("opening", formattedOpening);
+      formData.append("closing", formattedClosing);
 
       // Agregar los equipamientos seleccionados al FormData
-      formData.append('equipments', JSON.stringify(selectedEquipments));
+      formData.append("equipments", JSON.stringify(selectedEquipments));
 
       // Agregar las fotos al FormData
       for (let i = 0; i < photos.length; i++) {
@@ -82,7 +83,7 @@ const AddOfficeAdminPage = () => {
       const res = await fetch(
         `${import.meta.env.VITE_API_URL}/api/office/create`,
         {
-          method: 'POST',
+          method: "POST",
           headers: {
             Authorization: authToken,
           },
@@ -93,13 +94,13 @@ const AddOfficeAdminPage = () => {
       // Procesamiento de la respuesta
       const body = await res.json();
 
-      if (body.status === 'error') {
+      if (body.status === "error") {
         throw new Error(body.message);
       }
 
       // Mostrar mensaje de éxito y navegar a la página principal
       toast.success(body.message);
-      navigate('/');
+      navigate("/");
     } catch (err) {
       toast.error(err.message);
     }
@@ -120,114 +121,202 @@ const AddOfficeAdminPage = () => {
 
   // Función para manejar la selección de archivos
   const handleFile = (e) => {
-    const file = Array.from(e.target.files);
-    if (file) {
-      setPhotos(file); // Asigna las fotos seleccionadas al estado
-    }
+    const files = Array.from(e.target.files);
+    setPhotos((prevPhotos) => [...prevPhotos, ...files]);
+
+    // Crear previsualizaciones
+    const newPreviews = files.map((file) => ({
+      url: URL.createObjectURL(file),
+      file: file,
+    }));
+    setPhotoPreview((prevPreviews) => [...prevPreviews, ...newPreviews]);
   };
 
+  const removePhoto = (index) => {
+    setPhotos((prevPhotos) => prevPhotos.filter((_, i) => i !== index));
+    setPhotoPreview((prevPreviews) =>
+      prevPreviews.filter((_, i) => i !== index)
+    );
+  };
+
+  // Limpieza de las URLs de objeto creadas
+  useEffect(() => {
+    return () => {
+      photoPreview.forEach((preview) => URL.revokeObjectURL(preview.url));
+    };
+  }, [photoPreview]);
+
   return (
-    <div>
-      <h1>Añadir Oficina</h1>
-      <form onSubmit={handleAddSpace}>
-        <input
-          type='text'
-          placeholder='Nombre'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-        <input
-          type='text'
-          placeholder='Descripción'
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          required
-        />
-        <input
-          type='text'
-          placeholder='Dirección'
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-          required
-        />
-        <select
-          value={workspace}
-          onChange={(e) => setWorkspace(e.target.value)}
-          required
-        >
-          <option value=''>Seleccionar tipo de espacio</option>
-          <option value='OFFICE'>Oficina</option>
-          <option value='DESK'>Escritorio</option>
-        </select>
-        <input
-          type='number'
-          placeholder='Capacidad'
-          value={capacity}
-          onChange={(e) => setCapacity(e.target.value)}
-          required
-        />
-        <input
-          type='number'
-          placeholder='Precio'
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-          required
-        />
-        <label htmlFor='opening'>Horario de Apertura: </label>
-        <select
-          id='opening'
-          value={opening}
-          onChange={(e) => setOpening(e.target.value)}
-          required
-        >
-          {Array.from({ length: 24 }, (_, i) => (
-            <option key={i} value={`${i.toString().padStart(2, '0')}:00`}>
-              {`${i.toString().padStart(2, '0')}:00`}
-            </option>
-          ))}
-        </select>
-        <label htmlFor='closing'>Horario de Cierre: </label>
-        <select
-          id='closing'
-          value={closing}
-          onChange={(e) => setClosing(e.target.value)}
-          required
-        >
-          {Array.from({ length: 24 }, (_, i) => (
-            <option key={i} value={`${i.toString().padStart(2, '0')}:00`}>
-              {`${i.toString().padStart(2, '0')}:00`}
-            </option>
-          ))}
-        </select>
-        <label htmlFor='photos'>FOTOS:</label>
-        <input
-          type='file'
-          id='photos'
-          onChange={handleFile}
-          accept='image/jpeg, image/png'
-          multiple
-          required
-        />
+    <div className="add-office-container">
+      <h1>Crear Nuevo Espacio</h1>
+      <form onSubmit={handleAddSpace} className="add-office-form">
+        <div className="form-row">
+          <div className="form-group">
+            <label htmlFor="name">Nombre</label>
+            <input
+              id="name"
+              type="text"
+              placeholder="Nombre"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Descripción</label>
+            <input
+              id="description"
+              type="text"
+              placeholder="Descripción"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+            />
+          </div>
+        </div>
+
+        <div className="form-row">
+          <div className="form-group">
+            <label htmlFor="address">Dirección</label>
+            <input
+              id="address"
+              type="text"
+              placeholder="Dirección"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="workspace">Tipo de Espacio</label>
+            <select
+              id="workspace"
+              value={workspace}
+              onChange={(e) => setWorkspace(e.target.value)}
+              required
+            >
+              <option value="">Seleccionar tipo de espacio</option>
+              <option value="OFFICE">Oficina</option>
+              <option value="DESK">Escritorio</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="form-row">
+          <div className="form-group">
+            <label htmlFor="capacity">Capacidad</label>
+            <select
+              id="capacity"
+              value={capacity}
+              onChange={(e) => setCapacity(e.target.value)}
+              required
+            >
+              <option value="">Seleccionar capacidad</option>
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => (
+                <option key={num} value={num}>
+                  {num}
+                </option>
+              ))}
+              <option value="11+">11+</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="price">Precio</label>
+            <div className="price-input-container">
+              <input
+                id="price"
+                type="number"
+                placeholder="Precio"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                required
+              />
+              <span className="euro-symbol">€</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="form-row">
+          <div className="form-group">
+            <label htmlFor="opening">Horario de Apertura</label>
+            <select
+              id="opening"
+              value={opening}
+              onChange={(e) => setOpening(e.target.value)}
+              required
+            >
+              {Array.from({ length: 24 }, (_, i) => (
+                <option key={i} value={`${i.toString().padStart(2, "0")}:00`}>
+                  {`${i.toString().padStart(2, "0")}:00`}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="closing">Horario de Cierre</label>
+            <select
+              id="closing"
+              value={closing}
+              onChange={(e) => setClosing(e.target.value)}
+              required
+            >
+              {Array.from({ length: 24 }, (_, i) => (
+                <option key={i} value={`${i.toString().padStart(2, "0")}:00`}>
+                  {`${i.toString().padStart(2, "0")}:00`}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+        <div className="form-group photo-upload">
+          <label htmlFor="photos">Imagenes</label>
+          <div className="photo-upload-container">
+            <input
+              type="file"
+              id="photos"
+              onChange={handleFile}
+              accept="image/jpeg, image/png"
+              multiple
+            />
+            <div className="photo-upload-button">
+              <span>+</span>
+            </div>
+          </div>
+          <div className="photo-previews">
+            {photoPreview.map((preview, index) => (
+              <div key={index} className="photo-preview-item">
+                <img src={preview.url} alt={`Vista previa ${index + 1}`} />
+                <div
+                  className="photo-remove-button"
+                  onClick={() => removePhoto(index)}
+                >
+                  <span>-</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
 
         <h2>Equipamientos disponibles</h2>
+        <div className="equipment-list">
+          {Array.isArray(equipments) &&
+            equipments.map((equipment) => (
+              <div key={equipment.id} className="equipment-item">
+                <label>
+                  <input
+                    type="checkbox"
+                    value={equipment.id}
+                    onChange={handleEquipmentChange}
+                    checked={selectedEquipments.includes(equipment.id)}
+                  />
+                  {equipment.name}
+                </label>
+              </div>
+            ))}
+        </div>
 
-        {Array.isArray(equipments) &&
-          equipments.map((equipment) => (
-            <div key={equipment.id}>
-              <label>
-                <input
-                  type='checkbox'
-                  value={equipment.id} // Valor como número
-                  onChange={handleEquipmentChange}
-                  checked={selectedEquipments.includes(equipment.id)} // Comparar directamente como número
-                />
-                {equipment.name}
-              </label>
-            </div>
-          ))}
-
-        <button type='submit'>Crear Oficina</button>
+        <button type="submit">Crear Oficina</button>
       </form>
     </div>
   );
